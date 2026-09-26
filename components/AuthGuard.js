df794ac76355db2147984176f3d1cf05b1a1cc34
MuhammadRaviRayvansyah <revan110805@gmail.com>
2026-09-27 00:41:42 +0800

    error eslint

--- components/AuthGuard.js
+++ components/AuthGuard.js
@@ -12,6 +12,7 @@ export default function AuthGuard({ children }) {
   const [isAuthorized, setIsAuthorized] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsAuthorized(false);
 
     const token = getToken();
